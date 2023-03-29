4d18ec1d1ef8c06870d8c5eff72d22e15e97d9e8
Đông Ngô Anh <20010946@st.phenikaa-uni.edu.vn>
2023-03-29 11:56:46 +0700

    done

--- lib/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color tdRed = Color.fromARGB(255, 235, 103, 8);
-const Color tdBlue = Color.fromARGB(255, 28, 9, 236);
-
-const Color tdBlack = Color(0xFF3A3A3A);
-const Color tdGrey = Color(0xFF717171);
-
-const Color tdBGColor = Color.fromARGB(255, 206, 246, 248);
